27bd70bc220249ce719c036d06affb4d64810b93
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-16 20:18:20 +0900

    update

---
+++ pytorch_main.py
@@ -0,0 +1,137 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+
+class ConvNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=32,
+            kernel_size=(5, 5),
+            stride=(1, 1),
+            padding=(4, 4)
+        )
+        self.pool1 = nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(2, 2),
+            padding=(0, 0)
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(5, 5),
+            stride=(1, 1),
+            padding=(4, 4)
+        )
+        self.pool2 = nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(2, 2),
+            padding=(0, 0)
+        )
+        self.fc1 = nn.Linear(
+            in_features=7 * 7 * 64,
+            out_features=1024
+        )
+        self.fc2 = nn.Linear(
+            in_features=1024,
+            out_features=10
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = self.pool1(x)
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = self.pool2(x)
+        x = x.view(-1, 7 * 7 * 64)
+        x = self.fc1(x)
+        x = nn.functional.relu(x)
+        x = self.fc2(x)
+        x = nn.functional.log_softmax(x, dim=1)
+        return x
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.functional.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()
+            ))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += nn.functional.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)
+    ))
+
+
+if __name__ == "__main__":
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=datasets.MNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])
+        ),
+        batch_size=100,
+        shuffle=True,
+        **kwargs
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        dataset=datasets.MNIST(
+            root="data",
+            train=False,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+        batch_size=100,
+        shuffle=True,
+        **kwargs
+    )
+
+    model = ConvNet().to(device)
+    optimizer = optim.Adam(model.parameters())
+
+    for epoch in range(10):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+
+    torch.save(model.state_dict(), "mnist_convnet_model.pt")
---
+++ tensorflow_main.py
@@ -0,0 +1,161 @@
+import tensorflow as tf
+import numpy as np
+
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def conv_net(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    # MNIST images are 28x28 pixels, and have one color channel
+    input_layer = tf.reshape(
+        tensor=features["x"],
+        shape=[-1, 28, 28, 1]
+    )
+    # Convolutional Layer #1
+    # Computes 32 features using a 5x5 filter with ReLU activation.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 28, 28, 1]
+    # Output Tensor Shape: [batch_size, 28, 28, 32]
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu
+    )
+    # Pooling Layer #1
+    # First max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 28, 28, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 32]
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=[2, 2],
+        strides=[2, 2]
+    )
+    # Convolutional Layer #2
+    # Computes 64 features using a 5x5 filter.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 14, 14, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 64]
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu
+    )
+    # Pooling Layer #2
+    # Second max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 14, 14, 64]
+    # Output Tensor Shape: [batch_size, 7, 7, 64]
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv2,
+        pool_size=[2, 2],
+        strides=[2, 2]
+    )
+    # Flatten tensor into a batch of vectors
+    # Input Tensor Shape: [batch_size, 7, 7, 64]
+    # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+    pool2_flat = tf.layers.flatten(pool2)
+    # Dense Layer
+    # Densely connected layer with 1024 neurons
+    # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+    # Output Tensor Shape: [batch_size, 1024]
+    dense = tf.layers.dense(
+        inputs=pool2_flat,
+        units=1024,
+        activation=tf.nn.relu
+    )
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 10]
+    logits = tf.layers.dense(
+        inputs=dropout,
+        units=10
+    )
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=predictions
+        )
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer()
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step()
+        )
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op
+        )
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = dict(
+        accuracy=tf.metrics.accuracy(
+            labels=labels,
+            predictions=predictions["classes"]
+        )
+    )
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        eval_metric_ops=eval_metric_ops
+    )
+
+
+if __name__ == "__main__":
+
+    # Load training and eval data
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = np.asarray(mnist.train.images, dtype=np.float32)
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = np.asarray(mnist.test.images, dtype=np.float32)
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=conv_net,
+        model_dir="mnist_convnet_model"
+    )
+    # Set up logging for predictions
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors={"probabilities": "softmax_tensor"},
+        every_n_iter=100
+    )
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=10,
+        shuffle=True
+    )
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        hooks=[logging_hook]
+    )
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        batch_size=100,
+        num_epochs=1,
+        shuffle=False
+    )
+    eval_results = mnist_classifier.evaluate(
+        input_fn=eval_input_fn
+    )
+    print(eval_results)
