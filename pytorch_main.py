ee69a7b41e4dd7fc5e56ac2d717c5e6baef71ee0
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-16 20:40:29 +0900

    update

--- pytorch_main.py
+++ pytorch_main.py
@@ -58,40 +58,42 @@ class ConvNet(nn.Module):
         return x
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, optimizer, epochs):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = nn.functional.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()
-            ))
-            if batch_idx % 1000 == 0:
-                torch.save(model.state_dict(), "mnist_convnet_model.pt")
-
-
-def test(args, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
+    for epoch in range(epochs):
+        for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
             output = model(data)
-            test_loss += nn.functional.nll_loss(output, target, reduction='sum').item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)
-    ))
+            loss = nn.functional.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()
+                ))
+                if batch_idx % 1000 == 0:
+                    torch.save(model.state_dict(), "mnist_convnet_model.pt")
+
+
+def test(args, model, device, test_loader, epochs):
+    model.eval()
+    for epoch in range(epochs):
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += nn.functional.nll_loss(output, target, reduction='sum').item()
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)
+        ))
 
 
 if __name__ == "__main__":
@@ -133,9 +135,8 @@ if __name__ == "__main__":
 
     begin = time.time()
 
-    for epoch in range(10):
-        train(args, model, device, train_loader, optimizer, epoch)
-    test(args, model, device, test_loader)
+    train(args, model, device, train_loader, optimizer, 10)
+    test(args, model, device, test_loader, 1)
 
     end = time.time()
 
